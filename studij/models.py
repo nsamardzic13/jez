7d58152ba5c2f479aa8020296740344a55ad2d7a
nsamardzic13 <nsamardzic@riteh.hr>
2019-07-25 17:17:48 +0200

    Connection to Database via models

--- studij/models.py
+++ studij/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class kolegij(models.Model):
+class Studij(models.Model):
+    studij_id = models.CharField(max_length=10, primary_key=True)
+    studij_ime = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.studij_ime
+
+class Kolegij(models.Model):
     kolegij_id = models.CharField(max_length=10, primary_key=True)
-    kolegij_ime = models.CharField(max_length=40)
+    kolegij_ime = models.CharField(max_length=60)
+    semestar = models.SmallIntegerField()
+    studij_id = models.ForeignKey(Studij, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.kolegij_ime
-
-class semestar(models.Model):
-    semestar_br = models.PositiveSmallIntegerField()
--- studij/views.py
+++ studij/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+#not done
+def psvss(request):
+    return render(request, 'studij/psvss.html')
+
+def psvsb(request):
+    return render(request, 'studij/psvss.html')
+
+def psvse(request):
+    return render(request, 'studij/psvss.html')
+
+def psvsr(request):
+    return render(request, 'studij/psvss.html')
+
+def pstss(request):
+    return render(request, 'studij/psvss.html')
+
+def pstsb(request):
+    return render(request, 'studij/psvss.html')
+
+def pstse(request):
+    return render(request, 'studij/psvss.html')
+
+def dsvss(request):
+    return render(request, 'studij/psvss.html')
+
+def dsvsb(request):
+    return render(request, 'studij/psvss.html')
+
+def dsvse(request):
+    return render(request, 'studij/psvss.html')
+
+def dsvsr(request):
+    return render(request, 'studij/psvss.html')
