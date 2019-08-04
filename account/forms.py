9ba500d8ff479dce2d6035b935bba5e97ba31fd0
vrsaljk0 <majavrsaljko78@gmail.com>
2019-08-04 13:51:43 +0200

    created two forms for user reg

--- account/forms.py
+++ account/forms.py
@@ -1,64 +1,61 @@
 from django import forms
 import re
 from studij.models import Studij
+from django.contrib.auth.models import User
 from account.models import Student
 
-class RegistrationForm(forms.Form):
-    username = forms.CharField(max_length=30, required=True)
+from django.contrib.auth.forms import UserCreationForm
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(max_length=50, required=True)
     ime = forms.CharField(max_length=30, required=True)
     prezime = forms.CharField(max_length=30, required=True)
-    email = forms.EmailField(max_length=50, required=True)
-    studij_id = forms.ModelChoiceField(queryset=Studij.objects.all())
-    password = forms.CharField(max_length=30, widget=forms.PasswordInput)
-    password_repeat = forms.CharField(max_length=30, widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'ime', 'prezime', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        email_base, provider = email.split("@")
+        domain, extension = provider.split(".")
+
+        if not domain == "riteh":
+            raise forms.ValidationError("Potrebno je koristiti riteh mail adresu!")
+        return email
 
     def clean_ime(self):
-        ime = self.cleaned_data.get('ime')
+        ime = self.cleaned_data['ime']
         if not re.match("^[a-zA-Z0-9_]*$", ime):
             raise forms.ValidationError("Neispravan oblik imena!")
         return ime
 
     def clean_prezime(self):
-        prezime = self.cleaned_data.get('prezime')
+        prezime = self.cleaned_data['prezime']
 
-        if not re.match("^[a-zA-Z0-9_]*$", ime):
+        if not re.match("^[a-zA-Z0-9_]*$", prezime):
             raise forms.ValidationError("Neispravan oblik prezimena!")
         return prezime
 
-    def clean_username(self):
-        username = self.cleaned_data.get('username')
-        student_flag= Student.objects.filter(username=username).exists()
 
-        if(student_flag):
-            raise forms.ValidationError("Korisničko ime se već koristi!")
-        if not re.match("^[a-zA-Z0-9_]*$", username):
-            raise forms.ValidationError("Neispravan oblik korisničkog imena!")
-        return username
+    def save(self, commit=True):
+        user = super().save(commit=False)
 
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        email_base, provider = email.split("@")
-        domain, extension = provider.split(".")
-        email_flag = Student.objects.filter(email=email).exists()
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['ime']
+        user.last_name = self.cleaned_data['prezime']
 
-        if not domain == "riteh":
-            raise forms.ValidationError("Potrebno je koristiti valjanu riteh mail adresu!")
-        if(email_flag):
-            raise forms.ValidationError("Uneseni email se već koristi!")
-        return email
+        if commit:
+            user.save()
+        return user
 
-    def clean(self):
-        password1 = self.cleaned_data.get('password')
-        password2 = self.cleaned_data.get('password_repeat')
-
-        if(password1 != password2):
-            raise forms.ValidationError("Lozinke nisu jednake!")
-        if(password1 == password2 and len(password1) < 8):
-                raise forms.ValidationError("Lozinka mora imati minimalno 8 znakova!")
-        return password1
+class StudentProfileForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ('studij_id',)
 
 
 class LoginForm(forms.Form):
-    email = forms.CharField(max_length=50, required=True)
+    username = forms.CharField(max_length=50, required=True)
     password = forms.CharField(max_length=30, widget=forms.PasswordInput)
 
