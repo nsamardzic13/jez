b7c2b8005a4f7b0e9876444d22a7affe3d5e445a
vrsaljk0 <majavrsaljko78@gmail.com>
2019-08-09 20:12:12 +0200

    settings

--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import  User
 from django.utils import timezone
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
-from account.forms import RegistrationForm, LoginForm, StudentProfileForm
+from account.forms import RegistrationForm, StudentProfileForm, EditUserForm, EditStudentForm
 from django.contrib import messages
 from .models import Student, Moj_Kolegij
 from .models import Kolegij
@@ -29,9 +29,21 @@ def login_view(request):
 
 def settings_view(request):
     if request.user.is_authenticated:
-        return render(request, 'account/settings.html')
-    else:
-        return redirect('account:login')
+        if request.method == 'POST':
+            form = EditUserForm(request.POST, instance=request.user)
+            student_form = EditStudentForm(request.POST, request.FILES, instance=request.user.student)
+            if form.is_valid() and student_form.is_valid():
+                user_form = form.save()
+                student = student_form.save(commit=False)
+                student.user = user_form
+                student.save()
+                return redirect('account:settings')
+        else:
+            form = EditUserForm(instance=request.user)
+            student_form = EditStudentForm(instance=request.user.student)
+            context = {'form': form, 'student_form': student_form}
+            return render(request, "account/settings.html", context)
+
 
 def signup_view(request):
     if request.method == 'POST':
@@ -59,8 +71,6 @@ def mypage_view(request):
     #ispis sve moje kolegije
     username = request.session['username']
     svi_moji_kolegiji = Kolegij.objects.raw('select * from studij_kolegij, account_moj_kolegij where studij_kolegij.kolegij_id=account_moj_kolegij.kolegij_id and account_moj_kolegij.username= %s and studij_kolegij.studij_id_id=account_moj_kolegij.studij_id_id', [username])
-    for k in svi_moji_kolegiji:
-        print(k)
     context = {'svi_moji_kolegiji' : svi_moji_kolegiji}
     return render(request, "account/mypage.html", context)
 
