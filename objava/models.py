155ad1b9c85b6da47a14bfca545b315560417002
vrsaljk0 <majavrsaljko78@gmail.com>
2019-08-12 16:48:56 +0200

    file uploads

--- objava/models.py
+++ objava/models.py
@@ -10,10 +10,10 @@ from django import forms
 class Objava(models.Model):
     objava_id = models.AutoField(primary_key=True)
     username = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    kolegij_id = models.ForeignKey(Kolegij, on_delete=models.DO_NOTHING)
+    kolegij_id = models.CharField(max_length=500)
     tema = models.ForeignKey(Tema, on_delete=models.DO_NOTHING)
     date = models.DateTimeField(default=timezone.now)
-    attachment = models.FileField(upload_to= 'objava_att', null=True, blank=True)
+    attachment = models.FileField(upload_to= 'objava_att/', null=True, blank=True, max_length=500)
     tekst = models.TextField()
 
     def __str__(self):
--- objava/views.py
+++ objava/views.py
@@ -3,24 +3,24 @@ from .forms import ObjavaForm
 from studij.models import Kolegij
 from tema.models import Tema
 from objava.models import Objava, Objava_Likes
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def objava_view(request, studij_id, semestar_num, kolegij_id, tema_id):
-    # test = request.session['test']
-    username = request.session['username']
-    kol = Kolegij.objects.get(kolegij_id = kolegij_id)
-    sve_objave = Objava.objects.all().filter(kolegij_id = kol, tema_id = tema_id).order_by('date')
+    sve_objave = Objava.objects.all().filter(kolegij_id = kolegij_id, tema_id = tema_id).order_by('date')
     svi_lajkovi = Objava_Likes.objects.all()
 
     if request.method == 'POST':
-        form = ObjavaForm(data=request.POST)
+        form = ObjavaForm(request.POST, request.FILES)
         if form.is_valid():
             objava = form.save(commit=False)
             objava.username = request.user
-            objava.kolegij_id = Kolegij.objects.get(kolegij_id = kolegij_id)
+            kol = Kolegij.objects.get(kolegij_id=kolegij_id, studij_id=studij_id)
+            objava.kolegij_id = kol.kolegij_id
             objava.tema = Tema.objects.get(tema_id = tema_id)
-            # nadodati sta sve jos treba se pokupiti
             objava.save()
+            form = ObjavaForm()
     else:
         form = ObjavaForm()
+
     return render(request, 'objava/post.html', {'form': form, 'sve_objave':sve_objave, 'svi_lajkovi':svi_lajkovi})
