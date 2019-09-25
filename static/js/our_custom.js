9c1f8206cb8b485d45adf2499304e429b7a28cc2
nsamardzic13 <nsamardzic@riteh.hr>
2019-09-25 22:19:34 +0200

    Post answer improved

--- static/js/our_custom.js
+++ static/js/our_custom.js
@@ -1,5 +1,11 @@
 function copyText(user, text, datum) {
-    document.getElementById('id_tekst').value += "<<< " + user + " je " + datum + " napisao/la: " + text + ">>>";
+    document.getElementById('id_tekst').value += user + " je " + datum + " napisao/la:\n" + stripHtml(text) + "\n--------------------------------------------------\n";
+}
+
+function stripHtml(html)
+{
+   var c = document.getElementById(html);
+   return c.innerText;
 }
 
 function changeclass(element){
